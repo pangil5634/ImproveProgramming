e6b9a36176c0637287932734f5cdc360c477a56f
pangil5634 <oksk6681@naver.com>
2024-07-13 23:38:22 +0900

    edit : Array1 in JAVA_REVIEW

---
+++ Java_Review/실전코딩/Class3/Array1.java
@@ -0,0 +1,29 @@
+package Java_Review.실전코딩.Class3;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Array1 a1 = new Array1();
+        // a1.case1();
+        a1.case2();
+    }
+
+    public void case1() {
+        // 정상적인 선언
+        int[] numbers = new int[6];
+        // 비정상적인 선언
+        // int numbers[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public void case2() {
+        int[] numbers = { 10, 20, 30 };
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}

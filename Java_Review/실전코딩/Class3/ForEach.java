05ebee2db0dfa15fca8233e087a60158710d7601
pangil5634 <oksk6681@naver.com>
2024-07-15 23:38:43 +0900

    edit : For each in JAVA_REVIEW

---
+++ Java_Review/실전코딩/Class3/ForEach.java
@@ -0,0 +1,26 @@
+package Java_Review.실전코딩.Class3;
+
+public class ForEach {
+    public static void main(String[] args) {
+        ForEach foreach = new ForEach();
+        // foreach.case1();
+        foreach.case2();
+
+    }
+
+    public void case1() {
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = (int) (Math.random() * 10);
+        for (int value : numbers)
+            System.out.println(value);
+    }
+
+    public void case2() {
+        String[] str = { "Java", "C", "C++" };
+
+        for (String s : str)
+            System.out.println(s);
+        ;
+    }
+}

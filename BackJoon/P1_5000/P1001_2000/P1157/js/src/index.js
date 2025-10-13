cdb5ad556a115f2759abbbce49b25aa753766179
pangil5634 <oksk6681@naver.com>
2025-10-13 14:02:18 +0900

    refactor : update logic in printResult functions

--- BackJoon/P1_5000/P1001_2000/P1157/js/src/index.js
+++ BackJoon/P1_5000/P1001_2000/P1157/js/src/index.js
@@ -1,42 +1,32 @@
 function printResult(input) {
   // 1. 문자열을 분리한 배열을 생성한다.
-  const strArr = [];
-  for (let i = 0; i < input.length; i++) {
-    const char = input.substr(i, 1);
-    strArr.push(char.toLowerCase());
+  const strArr = input.toLowerCase().split("");
+
+  // 2. 각 단어마다 전체 개수를 구한다.
+  const countMap = {};
+
+  for (const char of strArr) {
+    countMap[char] = (countMap[char] || 0) + 1;
   }
 
-  // 2. 중복 없는 배열을 생성한다.
-  const strSet = new Set(strArr);
-
-  // 3. 각 단어마다 전체 개수를 구한다.
-  const charCountInfo = [];
-
-  Array.from(strSet).map((data, index) =>
-    charCountInfo.push({
-      char: data,
-      count: strArr.filter((element) => element == data).length,
-    })
-  );
-
-  // 4. 알파벳 중 가장 큰 횟수를 가진 숫자를 구한다.
   let maxCount = 0;
-  for (let i = 0; i < charCountInfo.length; i++) {
-    if (charCountInfo[i].count >= maxCount) {
-      maxCount = charCountInfo[i].count;
-    }
-  }
+  let result = "";
+  let isDuplicate = false;
 
-  // 5. 가장 많이 등장한 횟수와 일치한 알파벳을 구한다.
-  const maxChar = [];
-  for (let i = 0; i < charCountInfo.length; i++) {
-    if (charCountInfo[i].count == maxCount) {
-      maxChar.push(charCountInfo[i].char);
+  for (const char in countMap) {
+    const count = countMap[char];
+
+    if (count > maxCount) {
+      maxCount = count;
+      result = char;
+      isDuplicate = false;
+    } else if (count === maxCount) {
+      isDuplicate = true;
     }
   }
 
   // 6. 결과를 출력한다.
-  return maxChar.length === 1 ? maxChar[0].toUpperCase() : "?";
+  return isDuplicate ? "?" : result.toUpperCase();
 }
 
 function main() {
